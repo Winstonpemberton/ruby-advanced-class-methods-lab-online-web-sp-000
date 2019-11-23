0c4bcedbcdcca60644e4a2b13b5623771d9a535c
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-23 15:38:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     song = rows.collect do |row|
       name = rows[0]
       song_data = row.split(".")
-      artist_name = song_data[1]
+      artist_name = song_data[2]
       song = self.new
       song.artist_name = artist_name
       song.name = name
